1ec5baf7b75cbbdfa4533e50b6b18a40a0a8a75b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-14 02:40:18 +0000

    Deploy blunt/n2 to github.com/blunt/n2.git:master

---
+++ precache-manifest.eff083d0047c2ea9813b9506c8fa86c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b27f9011e05302c191f0a0af9f20f87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc758a29efb050df686c",
+    "url": "/static/css/main.2b35eb27.chunk.css"
+  },
+  {
+    "revision": "38e870d14ae03372a200",
+    "url": "/static/js/2.20d7a7d1.chunk.js"
+  },
+  {
+    "revision": "dc758a29efb050df686c",
+    "url": "/static/js/main.223eefb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
